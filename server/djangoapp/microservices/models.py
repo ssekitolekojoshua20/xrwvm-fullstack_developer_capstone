ef97c57fee3961a3d305b09fd1458b739c571f63
ssekitolekojoshua20 <ssekitolekojoshua20@gmail.com>
2026-08-12 17:57:45 -0400

    fixx lint

--- server/djangoapp/microservices/models.py
+++ server/djangoapp/microservices/models.py
@@ -4,14 +4,12 @@ from django.db import models
 # from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-
 # Create your models here.
-
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+# Name
+# Description
+# Any other fields you would like to include in car make model
+# __str__ method to print a car make object
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
@@ -30,7 +28,7 @@ class CarMake(models.Model):
 # Any other fields you would like to include in car model
 # __str__ method to print a car make object
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -40,12 +38,11 @@ class CarModel(models.Model):
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2026,
-        validators=[
+        validators = [
             MaxValueValidator(2026),
             MinValueValidator(2015)
         ])
     # Other fields as needed
 
     def __str__(self):
-        return self.name  # Return the name as the string representation
-
+        return self.name  
